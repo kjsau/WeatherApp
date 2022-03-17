55294ace64642de0185b9bf41485511f242b99e9
kjsau <101171603+kjsau@users.noreply.github.com>
2022-03-17 18:29:30 +0800

    changed main info to display on a card

--- source/app.js
+++ source/app.js
@@ -29,6 +29,7 @@ function formatDay(timestamp) {
 }
 
 function displayForecast(response) {
+  console.log(response.data);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class = "row">`;
